a51990088cdbf2ce82488534fba0aee7d761c2f3
yutwat <yutwat@gmail.com>
2019-02-24 08:14:48 +0900

    Add ability to make posts from administrator interface.

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
- AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
